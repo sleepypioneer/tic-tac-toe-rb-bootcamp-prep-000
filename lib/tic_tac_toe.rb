38eab661caeac6be1ca6689fb44ce54e92804353
Jessica Greene <jessica0greene@gmail.com>
2017-11-02 18:10:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,12 +49,34 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  valid = valid_move?(board, index)
+  value = current_player
+  valid = valid_move?(board, index, value)
   if valid == true
-    move(board, index)
+    move(board, index, value)
     display_board(board)
   elsif
     turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |place|
+    if place == "X"
+      count += 1
+    elsif place == "O"
+      count+=1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
   end
 end
 
