3b120d602530bab058be09d9866247f891fd1be9
Jessica Greene <jessica0greene@gmail.com>
2017-11-02 17:51:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,23 +9,53 @@ WIN_COMBINATIONS = [
   [2,4,6]  # diagonal right to left
 ]
 
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index
+def input_to_index(input)
+  input.to_i - 1
 end
 
-def move
+def move(board, index, value = "X")
+  board[index] = value
 end
 
-def position_taken?
 
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  elsif (board[index] == "X") || (board[index] == "O")
+    return true
+  end
 end
 
-def valid_move?
+def valid_move?(board, index)
+  taken = position_taken?(board, index)
+  if ((index >= 0) && (index <= 8)) && (taken == false)
+    return true
+  end
 end
 
-def turn
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  valid = valid_move?(board, index)
+  if valid == true
+    move(board, index)
+    display_board(board)
+  elsif
+    turn(board)
+  end
 end
 
 def won?(board)
