5ba297f084c570bac8f6ae93ea951ab9974d3ec7
Jessica Greene <jessica0greene@gmail.com>
2017-11-02 17:45:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,36 @@
+def WIN_COMBINATIONS
+end
 
+def display_board
+end
+
+def input_to_index
+end
+
+def move
+end
+
+def position_taken?
+  
+end
+
+def valid_move?
+end
+
+def turn
+end
+
+def won?
+end
+
+def full?
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
